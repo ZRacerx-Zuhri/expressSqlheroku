b89d6f39d84e524ea45f5bbe5d37fdbec2ad8e7d
ZRacerx-Zuhri <zsuryaputra89@gmail.com>
2019-07-31 10:51:02 +0700

    update

--- src/index.js
+++ src/index.js
@@ -10,4 +10,7 @@ server.use(express.json());
 
 server.use(userRouter);
 
+server.get("/", (req, res) => {
+  res.send("<h1>APA AJA</h1>");
+});
 server.listen(port, () => console.log("berhasil running di " + port));
